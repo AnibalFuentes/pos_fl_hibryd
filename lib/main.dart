7c08c9f5b4d8c3dd246cf2c23bc11610602c4c77
AnibalFuentes <anidev0308@gmail.com>
2026-04-22 17:12:33 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pos_fl_hibryd/main_widget.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
